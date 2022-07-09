0d440145c94073c86531107220a11b300e2dcd3b
Axel <canisma303@gmail.com>
2022-07-09 18:35:14 -0300

    new project added to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -64,13 +64,13 @@ const Work = () => {
 					<div style={{ backgroundImage: `url(${TodoApp})` }} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
 						{/* hover effect */}
 						<div className="opacity-0 group-hover:opacity-100">
-							<span className="text-2xl font-bold text-white tracking-wider">Empty</span>
+							<span className="text-2xl font-bold text-white tracking-wider">Todo React App</span>
 							<div className="pt-8 text-center">
 								<a href="https://axelzamora.github.io/todoApp-react/" target="_blank" rel="noreferrer">
-									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">+ Demo</button>
 								</a>
 								<a href="https://github.com/AxelZamora/todoApp-react" target="_blank" rel="noreferrer">
-									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">+ Code</button>
 								</a>
 							</div>
 						</div>

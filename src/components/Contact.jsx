961721c56d8b40f9608f152a54542fa25d3a3299
Axel <canisma303@gmail.com>
2022-07-13 18:19:47 -0300

    icons added in contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
 const Contact = () => {
 	return (
 		<div name="contact" className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
@@ -14,6 +18,21 @@ const Contact = () => {
 
 				<button className="text-white border-2 hover:bg-[#862c2d] hover:border-[#862c2d] px-4 py-3 my-8 mx-auto flex items-center">Let's talk</button>
 			</form>
+
+			<div>
+				<a className=" flex justify-between  w-auto p-2 text-gray-300 hover:scale-110 duration-500" href="https://www.linkedin.com/in/axel-zamora-8aa583ba/" target="_blank" rel="noreferrer">
+					<FaLinkedin size={30} />
+				</a>
+				<a className="flex justify-between  w-auto p-2 text-gray-300 hover:scale-110 duration-500" href="https://github.com/AxelZamora?tab=repositories" target="_blank" rel="noreferrer">
+					<FaGithub size={30} />
+				</a>
+				<a className="flex justify-between  w-auto p-2 text-gray-300 hover:scale-110 duration-500" href="https://mail.google.com/mail/?view=cm&source=mailto&to=axarzamora@gmail.com" target="_blank" rel="noreferrer">
+					<HiOutlineMail size={30} />
+				</a>
+				<a className="flex justify-between  w-auto p-2 text-gray-300 hover:scale-110 duration-500" href="https://drive.google.com/file/d/1SAIiuqxtkK3sW2doS6dCUT_5w7JoXS7b/view?usp=sharing" target="_blank" rel="noreferrer">
+					<BsFillPersonLinesFill size={30} />
+				</a>
+			</div>
 		</div>
 	)
 }
